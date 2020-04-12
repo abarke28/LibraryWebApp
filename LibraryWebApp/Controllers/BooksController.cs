04e105fbd8908679cefc664dac92f880506de75d
Alex Barker <abarke28@gmail.com>
2020-04-12 14:56:07 -0700

    First Controllers, models.

--- LibraryWebApp/Controllers/BooksController.cs
+++ LibraryWebApp/Controllers/BooksController.cs
@@ -9,11 +9,29 @@ namespace LibraryWebApp.Controllers
 {
     public class BooksController : Controller
     {
+        public IActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue) pageIndex = 1;
+            if (string.IsNullOrEmpty(sortBy)) sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
         public IActionResult Random()
         {
             Book book = new Book { Title = "Infinite Jest", Author = "Wallace, David Foster" };
 
             return View(book);
+        }
+
+        public IActionResult Edit(int id)
+        {
+            return Content(@"id=" + id);
+        }
+
+        public IActionResult Detail()
+        {
+            return Content("Author: " + "Wallace, David Foster");
         }
     }
 }
