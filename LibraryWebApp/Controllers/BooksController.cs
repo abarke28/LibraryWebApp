2d3060a8e08cb3e66339120da498fd2c5679422c
Alex Barker <abarke28@gmail.com>
2020-04-11 19:49:59 -0700

    Add project files.

---
+++ LibraryWebApp/Controllers/BooksController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LibraryWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LibraryWebApp.Controllers
+{
+    public class BooksController : Controller
+    {
+        public IActionResult Random()
+        {
+            Book book = new Book { Title = "Infinite Jest", Author = "Wallace, David Foster" };
+
+            return View(book);
+        }
+    }
+}
