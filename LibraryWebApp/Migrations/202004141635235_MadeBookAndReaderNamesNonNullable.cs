839146a1be94e9acbdc9925d51da963c29c43bf8
Alex Barker <abarke28@gmail.com>
2020-04-14 09:36:02 -0700

    Db tweaks.

---
+++ LibraryWebApp/Migrations/202004141635235_MadeBookAndReaderNamesNonNullable.cs
@@ -0,0 +1,20 @@
+﻿namespace LibraryWebApp.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class MadeBookAndReaderNamesNonNullable : DbMigration
+    {
+        public override void Up()
+        {
+            AlterColumn("dbo.Books", "Title", c => c.String(nullable: false));
+            AlterColumn("dbo.Readers", "Name", c => c.String(nullable: false));
+        }
+        
+        public override void Down()
+        {
+            AlterColumn("dbo.Readers", "Name", c => c.String());
+            AlterColumn("dbo.Books", "Title", c => c.String());
+        }
+    }
+}
--- LibraryWebApp/Models/Book.cs
+++ LibraryWebApp/Models/Book.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,7 @@ namespace LibraryWebApp.Models
     public class Book
     {
         public int Id { get; set; }
+        [Required]
         public string Title { get; set; }
         public string AuthorLastName { get; set; }
         public string AuthorFirstName { get; set; }
