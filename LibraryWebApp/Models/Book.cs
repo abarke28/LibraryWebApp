a70ec4ee9b096a159ba74891f20539324ee74ab1
Alex Barker <abarke28@gmail.com>
2020-04-15 16:26:40 -0700

    Add & Edit book functionality edited

--- LibraryWebApp/Models/Book.cs
+++ LibraryWebApp/Models/Book.cs
@@ -9,15 +9,29 @@ namespace LibraryWebApp.Models
     public class Book
     {
         public int Id { get; set; }
+
         [Required]
         public string Title { get; set; }
+
+        [Display(Name = "Author (Last Name)")]
         public string AuthorLastName { get; set; }
+
+        [Display(Name = "Author (First Name)")]
         public string AuthorFirstName { get; set; }
+
+        [Display(Name = "Publication Year")]
         public int PublishedYear { get; set; }
+        
+        [Display(Name = "Publication Month")]
         public int PublishedMonth { get; set; }
+        
         public string Synopsis { get; set; }
+        
         public Genre Genre { get; set; }
+        
+        [Display(Name = "Genre")]
         public int? GenreId { get; set; }
+        
         public int NumInStock { get; set; }
 
         public virtual ICollection<Reader> Readers { get; set; }
