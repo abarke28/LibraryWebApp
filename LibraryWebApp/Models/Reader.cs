3268ac0f38374d2b58bf36ff5000c519f0ee6b45
Alex Barker <abarke28@gmail.com>
2020-04-15 12:35:01 -0700

    Added Display Data Annotations to  Reader class

--- LibraryWebApp/Models/Reader.cs
+++ LibraryWebApp/Models/Reader.cs
@@ -9,10 +9,17 @@ namespace LibraryWebApp.Models
     public class Reader
     {
         public int Id { get; set; }
+
         [Required]
+        [Display(Name = "Reader (Full Name)")]
         public string Name { get; set; }
+
         public MembershipType MembershipType { get; set; }
+
+        [Display(Name = "Membership Type")]
         public int MembershipTypeId { get; set; }
+
+        [Display(Name = "Subscribed to Newsletter?")]
         public bool IsSubscribedToNewsletter { get; set; }
 
         public virtual ICollection<Book> Books { get; set; }
