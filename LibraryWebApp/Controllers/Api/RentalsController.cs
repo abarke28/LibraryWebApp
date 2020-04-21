bc71ee528b50a2139fce933c9c1ae12e950c06c7
Alex Barker <abarke28@gmail.com>
2020-04-21 12:13:46 -0700

    Added rental API

--- LibraryWebApp/Controllers/Api/RentalsController.cs
+++ LibraryWebApp/Controllers/Api/RentalsController.cs
@@ -31,17 +31,46 @@ namespace LibraryWebApp.Controllers.Api
 
             var reader = _context.Readers.FirstOrDefault(r => r.Id == rentalDto.ReaderId);
 
-            var book = _context.Books.FirstOrDefault(b => b.Id == rentalDto.ReaderId);
+            // Reader not found
+            if (reader == null) return BadRequest();
 
-            // If Id not valid
-            if (book == null) return BadRequest();
+            var books = _context.Books.Where(b => rentalDto.BookIds.Contains(b.Id));
 
-            // If no copies available
-            if (book.NumInStock < 1) return BadRequest();
+            // No Books found
+            if (books == null) return BadRequest();
 
-            // TODO: Implementation Logic
+            var unavailable = new List<Book>();
 
-            return Ok();
+            foreach (Book book in books)
+            {
+                if (book == null) break;
+
+                // Book is in stock, add rental
+                if (book.NumInStock > 0)
+                {
+                    // Book is in stock, add rental
+                    _context.Rentals.Add(new Rental()
+                    {
+                        Reader = reader,
+                        Book = book,
+                        DateRented = DateTime.Today
+                    });
+                    break;
+                }
+                // Book not in stock, add to unavailable list to return to user
+                else
+                {
+                    unavailable.Add(book);
+                }
+            }
+
+            _context.SaveChanges();
+
+            // No rentals were unavailable
+            if (unavailable.Count == 0) return Ok();
+
+            // Some rentals were unavailable
+            return Ok(unavailable);
         }
     }
 }
